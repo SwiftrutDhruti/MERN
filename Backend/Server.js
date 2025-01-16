568d6443551a313e7a3442f64c4e4f898fec0cc2
SwiftrutDhruti <dhruti.patel@swfitrut.com>
2025-01-16 23:50:33 +0530

    Cloudinary

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,3 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 8000;
+connectDB()
+connectCloudinary()
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+res.send('API WORKING GREAT')
+})
+
+
+app.listen(port,()=> console.log("Server listening on port", port))
